4f02ff9231cd70d7c20bbd818bc81961523d0a0a
James Trauger <jamestrauger02@gmail.com>
2024-10-28 12:21:58 -0400

    login form event handler

--- frontend/react/pages/Login.tsx
+++ frontend/react/pages/Login.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { FormEvent, FormEventHandler } from 'react';
+
+interface LoginElements extends HTMLFormControlsCollection {
+  username: HTMLInputElement;
+  password: HTMLInputElement;
+}
+
+interface LoginForm extends HTMLFormElement {
+  readonly elements: LoginElements;
+}
 
 export default function Login() {
+  
+  const handleSubmit = (event: FormEvent<LoginForm>) => {
+    event.preventDefault();
+    const elements = event.currentTarget.elements;
+    const data = {
+      username: elements.username.value,
+      password: elements.password.value,
+    };
+    console.log(data);
+    // send post request
+  };
+  
   return (
-    <div className="login">
+    <form onSubmit={handleSubmit} className="login">
         <h2>Login</h2>
         <label>Username:</label>
         <input
+            id="username"
             type="text"
             required
         />
         <label>Password</label>
         <input
+            id="password"
             type="text"
             required
         />
-    </div>
-  )
+        <button type="submit">Submit</button>
+    </form>
+  );
 }
