ea497d2f74964f64cd1ce365fa7e9e1e8d4a2f81
James Trauger <jamestrauger02@gmail.com>
2025-12-22 14:21:01 -0500

    find a user by username

--- api/src/main/java/com/jamestrauger/recipouir/repositories/UserRepository.java
+++ api/src/main/java/com/jamestrauger/recipouir/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package com.jamestrauger.recipouir.repositories;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import com.jamestrauger.recipouir.models.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    Optional<User> findByUsername(String username);
 }
