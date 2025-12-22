eb3bcadfa06bb38c59fa5cb4e4ebf2243b1c1df8
James Trauger <jamestrauger02@gmail.com>
2025-12-22 14:43:48 -0500

    login post mapping

--- api/src/main/java/com/jamestrauger/recipouir/controllers/AuthenticationController.java
+++ api/src/main/java/com/jamestrauger/recipouir/controllers/AuthenticationController.java
@@ -1,17 +1,45 @@
 package com.jamestrauger.recipouir.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.jamestrauger.recipouir.models.dto.LoginRequest;
 import com.jamestrauger.recipouir.repositories.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/auth")
 public class AuthenticationController {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
 
-    private AuthenticationController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest,
+            HttpServletRequest request) {
+        Authentication authenticationRequest = UsernamePasswordAuthenticationToken
+                .unauthenticated(loginRequest.getUsername(), loginRequest.getPassword());
+        Authentication authenticationResponse =
+                this.authenticationManager.authenticate(authenticationRequest);
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationResponse);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+        return new ResponseEntity<>("User signed in successfully", HttpStatus.OK);
     }
-
 }
