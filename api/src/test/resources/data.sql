dc356bfe9906f399241177db33ec8f613493d14c
James Trauger <jamestrauger02@gmail.com>
2025-12-30 10:54:30 -0500

    added hashed password when inserting user

--- api/src/test/resources/data.sql
+++ api/src/test/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (id, username, first_name, last_name) VALUES (47, 'asoiaf', 'ned', 'stark');
+INSERT INTO users (id, username, first_name, last_name, password) VALUES (47, 'asoiaf', 'ned', 'stark', '$2a$10$SaU8AfqGvUoeLIaJ2W7KY.e3ybJ5RC9mkQxiwFN3tBqu2Jj1vb.XW');
 INSERT INTO recipe (id, title, created_by, servings) VALUES (99, 'Cookies', 47, 5);
 INSERT INTO step (number, recipe_id, description) VALUES (1, 99, 'mix the ingredients');
 INSERT INTO step (number, recipe_id, description) VALUES (2, 99, 'bake at 350');
